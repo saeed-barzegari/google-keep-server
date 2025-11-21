0607ea18db3ca0e088bcb4a2893a6a823226542f
saeed.b <barzegarisaeed.81@gmail.com>
2025-11-21 20:47:14 +0330

    Feat: Token base authentication
    add authtoken to install apps and set settings to use token for authentication

--- backend/settings.py
+++ backend/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'drf_yasg',
     'corsheaders',
     'notes.apps.NotesConfig',
@@ -91,6 +92,12 @@ DATABASES = {
     }
 }
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
